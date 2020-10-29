55f8ae8e7257288253f653c4304594813623c02b
NickNighthawk <superquestarcade@gmail.com>
2020-10-29 17:51:50 +1100

    Isolate subscene loader system

---
+++ TrashMultiplayerPtype/Assets/Scripts/Authoring/EnableSubsceneLoadingAuth.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class EnableSubsceneLoadingAuth : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new EnableSubsceneLoading());
+    }
+}
--- TrashMultiplayerPtype/Assets/Scripts/SubsceneLoader.cs
+++ TrashMultiplayerPtype/Assets/Scripts/SubsceneLoader.cs
@@ -1,48 +1,49 @@
-﻿//using UnityEngine;
-//using Unity.Entities;
-//using Unity.Mathematics;
-//using Unity.Scenes;
-//using Unity.Transforms;
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Scenes;
+using Unity.Transforms;
 
 
-//public class SubsceneLoader : ComponentSystem
-//{
-//    private SceneSystem sceneSystem;
+public class SubsceneLoader : ComponentSystem
+{
+    private SceneSystem sceneSystem;
 
-//    protected override void OnCreate()
-//    {
-//        sceneSystem = World.GetOrCreateSystem<SceneSystem>();
-//    }
+    protected override void OnCreate()
+    {
+        sceneSystem = World.GetOrCreateSystem<SceneSystem>();
+        RequireSingletonForUpdate<EnableSubsceneLoading>();
+    }
 
-//    protected override void OnUpdate()
-//    {
-//        Entities
-//            .WithAll<Player>() //took out movable here
-//            .ForEach((ref Translation translation) =>
-//            {
-//                float loadDistance = SubsceneReferences.instance.loadDistance;
-//                foreach (SubScene subScene in SubsceneReferences.instance.subScenes)
-//                {
-//                    if (math.distance(translation.Value, subScene.transform.position) < loadDistance)
-//                    {
-//                        LoadSubScene(subScene);
-//                    }
-//                    else
-//                    {
-//                        UnloadSubScene(subScene);
-//                    }
-//                }
+    protected override void OnUpdate()
+    {
+        Entities
+            .WithAll<Player>() //took out movable here
+            .ForEach((ref Translation translation) =>
+            {
+                float loadDistance = SubsceneReferences.instance.loadDistance;
+                foreach (SubScene subScene in SubsceneReferences.instance.subScenes)
+                {
+                    if (math.distance(translation.Value, subScene.transform.position) < loadDistance)
+                    {
+                        LoadSubScene(subScene);
+                    }
+                    else
+                    {
+                        UnloadSubScene(subScene);
+                    }
+                }
                 
-//            });
-//    }
+            });
+    }
 
-//    private void LoadSubScene(SubScene subScene)
-//    {
-//        sceneSystem.LoadSceneAsync(subScene.SceneGUID);
-//    }
+    private void LoadSubScene(SubScene subScene)
+    {
+        sceneSystem.LoadSceneAsync(subScene.SceneGUID);
+    }
     
-//    private void UnloadSubScene(SubScene subScene)
-//    {
-//        sceneSystem.UnloadScene(subScene.SceneGUID);
-//    }
-//}
+    private void UnloadSubScene(SubScene subScene)
+    {
+        sceneSystem.UnloadScene(subScene.SceneGUID);
+    }
+}
