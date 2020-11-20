d59a56d408464c4ce49cebd24b00826a825e10a5
Hayley <hayley25.k25@gmail.com>
2020-11-20 16:33:58 +1100

    Created player disconnect system

---
+++ TrashMultiplayerPtype/Assets/Scripts/Systems/HandleDisconnectSystem.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.NetCode;
+
+
+[UpdateInGroup(typeof(ServerSimulationSystemGroup))]
+public class HandleDisconnectSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        // var commandBuffer = new EntityCommandBuffer(Allocator.Temp);
+        
+        Entities
+            .WithNone<SendRpcCommandRequestComponent>()
+            .ForEach((Entity e, ref NetworkStreamDisconnected disconnect, ref NetworkIdComponent netID) =>
+            {
+                NetworkStreamDisconnectReason reason = disconnect.Reason;
+                int sourceID = netID.Value;
+                Debug.Log($"Player {sourceID} has disconnected due to reason: {reason}");
+
+                Entities.ForEach((Entity ent, ref PlayerID playerId) =>
+                {
+                    if (playerId.playerID == sourceID)
+                    {
+                        //commandBuffer.DestroyEntity(ent);
+                        PostUpdateCommands.DestroyEntity(ent);
+                    }
+                });
+            });
+        //commandBuffer.Playback(EntityManager);
+    }
+}
+
+
