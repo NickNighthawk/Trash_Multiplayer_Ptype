e2ef67a575538af92a015a2468ac98e550b64bc4
Hayley <hayley25.k25@gmail.com>
2020-11-30 17:25:16 +1100

    pathfinding in DOTS 2D no visuals

---
+++ TrashMultiplayerPtype/Assets/Scripts/PathFinding/MonoGridTesing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoGridTesing : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        GridTest grid = new GridTest(2, 2, 2);
+    }
+
+}
